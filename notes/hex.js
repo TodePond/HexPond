a02504ee345f932f0667c912a299e137350e4132
Luke Wilson <l2wilson94@gmail.com>
2021-04-16 17:25:21 +0100

    notes

---
+++ notes/hex.js
@@ -0,0 +1,22 @@
+
+
+element Sand any(xz.reflections) {
+	
+	@ => _
+	_    @
+	
+	@  => _
+	 _     @
+	
+}
+
+element Water any(xz.reflections) {
+	@ => _
+	_    @
+	
+	@  => _
+	 _     @
+	 
+	 _     @
+	@  => _
+}
